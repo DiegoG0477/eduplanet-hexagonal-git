529a272af6a6da1049766c328d2ae701bd7ee2b7
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-04-16 10:20:10 -0600

    login bug fixed

--- src/user/infraestructure/controllers/LoginController.ts
+++ src/user/infraestructure/controllers/LoginController.ts
@@ -10,11 +10,18 @@ export class LoginController {
         try {
             const token = await this.loginUseCase.run(email, password);
 
-            return res.status(200).json({
-                status: "success",
-                message: "User logged in successfully",
-                token
-            });
+            if(token){
+                return res.status(200).json({
+                    status: "success",
+                    message: "User logged in successfully",
+                    token
+                });
+            }else{
+                return res.status(400).json({
+                    status: "error",
+                    message: "invalid credentials",
+                });
+            }
         } catch (error) {
             return res.status(400).json({
                 status: "error",
